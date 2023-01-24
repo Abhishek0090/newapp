2ad533697ad9bd128780f8b28923e6f6795c60b7
Abhishek0090 <palabhishek411@gmail.com>
2023-01-24 12:07:46 +0530

    added api and context

--- App.js
+++ App.js
@@ -6,7 +6,7 @@ import InshortsTab from './components/InshortsTab';
 export default function App() {
 
   return (
-    <View style={{ ...styles.container,  }}>
+    <View style={{ ...styles.container, backgroundColor: '#282C35' }}>
       <InshortsTab />
     </View>
   );
@@ -16,6 +16,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     marginTop: StatusBar.currentHeight
-  },
-
+  }
 });
--- components/TopNavigation.js
+++ components/TopNavigation.js
@@ -1,48 +1,93 @@
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
-import React from 'react'
-import { MaterialCommunityIcons } from '@expo/vector-icons'
+import React, { useContext } from "react";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
 import { SimpleLineIcons } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
+// import { NewsContext } from "../API/Context";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 const TopNavigation = ({ index, setIndex }) => {
-    return (
-        <View style={{ ...styles.container, backgroundColor: '#282c35' }}>
-            {
-                index === 0 ?
-                    <TouchableOpacity style={styles.left}>
-                        <Text style={{ ...styles.text, color: 'lightgrey' }}>
-                            <MaterialCommunityIcons
-                                name='theme-light-dark'
-                                size={24}
-                                color="#007FFF"
-                            />
-                        </Text>
-                    </TouchableOpacity>
-                    : (
-                        <TouchableOpacity>
-                            <SimpleLineIcons name="arrow-left" size={15} color="#007FFF" />
-                            <Text
-                                style={{ ...styles.text, color: "black" }}
-                            >
-                                Discover
-                            </Text>
-                        </TouchableOpacity>
-                    )
-            }
-        </View>
-    )
-}
+    // const { darkTheme, setDarkTheme, fetchNews } = useContext(NewsContext);
 
+    const darkTheme = false;
+
+   
+
+    return (
+        <View
+            style={{
+                ...styles.container,
+                backgroundColor: darkTheme ? "#282C35" : "white",
+            }}
+        >
+            {index === 0 ? (
+                <TouchableOpacity
+                    onPress={() => setDarkTheme(!darkTheme)}
+                    style={styles.left}
+                >
+                    <Text
+                        style={{ ...styles.text, color: darkTheme ? "lightgrey" : "black" }}
+                    >
+                        <MaterialCommunityIcons
+                            name="theme-light-dark"
+                            size={24}
+                            color="#007FFF"
+                        />
+                    </Text>
+                </TouchableOpacity>
+            ) : (
+                <TouchableOpacity
+                    style={styles.left}
+                    onPress={() => setIndex(index === 0 ? 1 : 0)}
+                >
+                    <SimpleLineIcons name="arrow-left" size={15} color="#007FFF" />
+                    <Text
+                        style={{ ...styles.text, color: darkTheme ? "lightgrey" : "black" }}
+                    >
+                        Discover
+                    </Text>
+                </TouchableOpacity>
+            )}
+
+            <Text style={{ ...styles.center, color: darkTheme ? "white" : "black" }}>
+                {index ? "All News" : "Discover"}
+            </Text>
+            {index ? (
+                <TouchableOpacity
+                    style={styles.right}
+                    onPress={() => fetchNews("general")}
+                >
+                    <Text style={styles.text}>
+                        <AntDesign name="reload1" size={24} color="#007FFF" />
+                    </Text>
+                </TouchableOpacity>
+            ) : (
+                <TouchableOpacity
+                    style={styles.left}
+                    onPress={() => setIndex(index === 0 ? 1 : 0)}
+                >
+                    <Text
+                        style={{ ...styles.text, color: darkTheme ? "white" : "black" }}
+                    >
+                        All News
+                    </Text>
+                    <SimpleLineIcons name="arrow-right" size={15} color="#007FFF" />
+                </TouchableOpacity>
+            )}
+        </View>
+    );
+};
+
+export default TopNavigation;
 
 const styles = StyleSheet.create({
     container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: "row",
+        justifyContent: "space-between",
         padding: 10,
-        alignItems: 'center',
-        borderBottomColor: 'black',
-        borderBottomWidth: 0.5
+        alignItems: "center",
+        borderBottomColor: "black",
+        borderBottomWidth: 0.5,
     },
     center: {
         paddingBottom: 6,
@@ -65,7 +110,4 @@ const styles = StyleSheet.create({
         width: 80,
         alignItems: "flex-end",
     },
-})
-
-
-export default TopNavigation
+});
